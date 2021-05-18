29aef5f69bc2d49aa9cda05bb55181b8e19d0d2e
UsernameSeeking <nshuan8@gmail.com>
2021-05-18 17:38:17 +0700

    3

--- game1.js
+++ game1.js
@@ -6,10 +6,18 @@ function game1() {
 	var page1 = document.getElementById('page1');
 	var page2 = document.getElementById('page2');
 	var ptext = document.getElementById('playGame1_text');
+	var plbrd = document.getElementById('playboard');
+	var ingrd = document.getElementById('innerground');
 	if (page1.style.display == "block") {
 		page1.style.display = "none";
 		page2.style.display = "block";
 		ptext.innerHTML = "Back";
+		if (innerWidth >= innerHeight) {
+			plbrd.style.width = "300px";
+			plbrd.style.height = "300px";
+			ingrd.style.width = "240";
+			ingrd.style.height = "240";
+		}
 	}
 	else {
 		if (page1.style.display == "none") {
