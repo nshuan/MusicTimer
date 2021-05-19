673babb765d41c894e9c3cae0c5dda25c7dda4fd
UsernameSeeking <nshuan8@gmail.com>
2021-05-19 10:56:24 +0700

    5

--- game1.js
+++ game1.js
@@ -20,7 +20,19 @@ function game1() {
 		}
 	}
 	else {
-		startGame();
+		if (ptext.innerHTML == "Play again") {
+			ptext.innerHTML = "Start";
+			var tiles = document.getElementsByClassName('tile');
+			var cover = document.getElementById('cover');
+			var congr = document.getElementsByClassName('countdown_number');
+			cover.remove();
+			congr[1].remove();
+			for (var i = 0; i < 16; i++) {
+				tiles[i].style.backgroundColor = "white";
+				choice[i].style.backgroundColor = "white";
+			}
+		}
+		else startGame();
 	}
 }
 
@@ -31,11 +43,16 @@ function back() {
 	ptext.innerHTML = "Play Game";
 }
 
-var a = 3;
+var tilsa = [];
+var choice = document.getElementsByClassName('btile');
+var p_ans = [];
 
 function startGame() {
 	var grd = document.getElementsByClassName('ground');
 	var countnum = document.getElementById('countdown_number');
+	if (grd[1].style.display == "none") grd[1].style.display = "block";
+	tilsa = [];
+	p_ans = [];
 	for (var i = 0; i < grd.length; i++) {
 		grd[i].style.display = "none";
 	}
@@ -47,13 +64,69 @@ function startGame() {
 function gamePlay() {
 	var tiles = document.getElementsByClassName('tile');
 	var grd = document.getElementsByClassName('ground');
-	var tilsa = [];
 	for (var i = 0; i < 8; i++) {
 		var sub = Math.floor(Math.random()*15);
 		while (tiles[sub].style.backgroundColor == "blue") {sub = Math.floor(Math.random()*15);}
 		tiles[sub].style.backgroundColor = "blue";
 		tilsa.push(sub);
 	}
-	setTimeout(function(){for (var j = 0; j < tilsa.length; j++) tiles[tilsa[j]].style.backgroundColor = "white";}, 2500);
+	setTimeout(function(){grd[1].style.display = "none";}, 2500);
+}
+
+function checkchoice(no) {
+	var plb = document.getElementById('playboard');
+	choice[no].style.backgroundColor = "blue";
+	p_ans.push(no);
+	if (p_ans.length == 8) {
+		var cover = document.createElement('div');
+		cover.className = "ground";
+		cover.id = "cover";
+		plb.appendChild(cover);
+		checkans();
+	}
 	console.log(tilsa);
+	console.log(p_ans.length);
+	console.log(p_ans);
+}
+
+function checkans() {
+	var plb = document.getElementById('playboard');
+	var cover = document.getElementById('cover');
+	var congr = document.createElement('p');
+	var tiles = document.getElementsByClassName('tile')
+	var ptext = document.getElementById('playGame1_text');
+	congr.className = "countdown_number";
+	tilsa.sort();
+	p_ans.sort();
+	console.log(tilsa);
+	console.log(p_ans);
+	if (compare_arrays(tilsa, p_ans) == true) {
+		cover.style.backgroundColor = "black";
+		cover.style.opacity = 0.4;
+		congr.innerHTML = "Correct!";
+		congr.style.color = "green";
+		congr.style.fontSize = "80px";
+		congr.style.opacity = "0.8";
+		plb.appendChild(congr);
+	}
+	else {
+		cover.style.backgroundColor = "black";
+		cover.style.opacity = 0.4;
+		congr.innerHTML = "Incorrect!";
+		congr.style.color = "red";
+		congr.style.fontSize = "80px";
+		congr.style.opacity = "0.8";
+		plb.appendChild(congr);
+	}
+	ptext.innerHTML = "Play again";
+}
+
+function compare_arrays(m, n) {
+	if (m.length != n.length) return false;
+	else {
+		for (var i = 0; i < m.length; i++) {
+			if (m[i] != n[i]) return false;
+		}
+		return true;
+	}
 }
